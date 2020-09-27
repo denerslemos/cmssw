7770b6740ff399f3c458c6993f5dd0a25bd89dfd
ddesouza <dener.lemos@cern.ch>
2020-09-27 18:11:00 +0200

    Include HF Filters as struct in MiniAOD content

---
+++ DataFormats/HeavyIonEvent/interface/HFFilterStruct.h
@@ -0,0 +1,16 @@
+#ifndef HEAVYIONEVENT_HFFILTERSTRUCT_H
+#define HEAVYIONEVENT_HFFILTERSTRUCT_H
+
+namespace pat
+{
+  struct HFFilterStruct
+  {
+    int numMinHFTowers2;
+    int numMinHFTowers3;
+    int numMinHFTowers4;
+    int numMinHFTowers5;
+  };
+}
+
+#endif
+
